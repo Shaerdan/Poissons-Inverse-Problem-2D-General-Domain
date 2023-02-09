eecb962fdab027c00ebdcf80a90d498909192179
Shaerdan <Shaerdan@gmail.com>
2023-02-09 22:15:37 +0000

    updated lasso

--- FE_forward_solution_proto.m
+++ FE_forward_solution_proto.m
@@ -1,18 +1,30 @@
 clc; clear all; close all;
 smoothsteps = 1;
 rmax = 1;
-rmin = 0.8;
-n_mesh_bc = 180;
+rmin = 0.7;
+n_vertices_bc_geometry = 380;
+H_max_FEM = 0.5;
+eps = 5*1e-2; % margin for the Kronecker delta function
 l_plot_smoothing = 1;
 l_plot_conformal = 0;
 l_plot_loop = 0;
 l_grad_test = 1;
 curve_num = 1;
 n_mesh_bulk = 400;
-N_source = 4;
+N_source = 2;
+var_noise = 0;
+lambda_FEM = 0.1;
+lambda_inv_tikh = 5*1e-1;
+lambda_inv_lasso = 1e-5;
+reg_method = 2; % 1 for Tikhonov, 2 for lasso
+if reg_method == 1
+    lambda_inv = lambda_inv_tikh;
+elseif reg_method == 2
+    lambda_inv = lambda_inv_lasso;
+end
 
 for itest = 1:curve_num
-    [x_curve,y_curve] = randomsmoothcurveB2(smoothsteps,rmax,rmin,n_mesh_bc,l_plot_smoothing);
+    [x_curve,y_curve] = randomsmoothcurveB2(smoothsteps,rmax,rmin,n_vertices_bc_geometry,l_plot_smoothing);
     hold on;
 end
 bc_curve = [x_curve;y_curve];
@@ -24,7 +36,7 @@ S_true = S_true - S_mean;
 
 
 if l_plot_conformal == 1
-    [scaling_factor,w] = conformalscaling(x_curve,y_curve,n_mesh_bc);
+    [scaling_factor,w] = conformalscaling(x_curve,y_curve,n_vertices_bc_geometry);
     
     phi_bc_B2 =  soln_bc_B2(S_true,Zi,z_ref,N_source);
     phi_forward = scaling_factor.*phi_bc_B2;
@@ -41,16 +53,18 @@ tri = delaunayTriangulation([x_curve, y_curve]);
 figure(901)
 triplot(tri);hold on;
 scatter(x_source,y_source,'r*');
-var_noise = 0;
-lambda = 0.1;
+
 model = createpde();
 stlwrite(tri,'tritext2D.stl','text')
 geo = importGeometry(model,"tritext2D.stl");
 
 g = @(region,state)gfun ;
-mesh=generateMesh(model,"Hmax",.05,"GeometricOrder","linear");
+% mesh=generateMesh(model,"Hmax",H_max_FEM,"GeometricOrder","linear");
+mesh=generateMesh(model,"Hgrad",1.1,"GeometricOrder","linear");
+
 figure(9090)
-pdegplot(model.Geometry)
+[p,e,t] = meshToPet(mesh);
+pdeplot(p,e,t)
 disp(strcat('Number of Nodes = ','',num2str(length(mesh.Nodes))));
 applyBoundaryCondition(model,"neumann", ...
     "Face",1:model.Geometry.NumFaces, ...
@@ -66,7 +80,6 @@ phi = @(xi, eta) [(1 - xi - eta), xi, eta];
 conv_kernel = [];
 tol = 1e-5;
 i_sigma = 1;
-eps = 1*1e-1;
 f = @(location,state)fsource(location,state,S_true,x_source,y_source,N_source,eps,conv_kernel);
 specifyCoefficients(model,"m",0,...
     "d",0,...
@@ -78,12 +91,9 @@ noise = sqrt(var_noise)*randn(size(bndNodes));
 
 cond(FEMn.Kc)
 
-tikh = FEMn.Kc'*FEMn.Kc + lambda*speye(size(FEMn.Kc));
+tikh = FEMn.Kc'*FEMn.Kc + lambda_FEM*speye(size(FEMn.Kc));
 FcKinv = tikh\FEMn.Kc'*FEMn.Fc;
 soln_FE_n_tikh(:,1) = FEMn.B*(FcKinv) + FEMn.ud;
-Data_Bc = soln_FE_n_tikh(bndNodes,1) + noise;
-figure(10)
-plot(Data_Bc)
 
 if l_plot_loop == 1
     figure(plot_num)
@@ -103,19 +113,23 @@ if l_plot_loop == 1
 end
 
 A = FEMn.Kc;
-% A_Tikh = FEMn.Kc'*FEMn.Kc + lambda*speye(size(FEMn.Kc));
-% Data_Fc_Tikh = FEMn.Kc'*Data_Fc;
-X0 = rand(size(soln_FE_n_tikh));
-
+A_tikh_inv = tikh\FEMn.Kc';
+nndoes = length(A_tikh_inv); nbcnodes = length(bndNodes);
+H_obs = zeros(nbcnodes,nndoes);
+H_obs_diag = ones(nbcnodes,1); % observation pattern on the boundary
+H_obs(1:nbcnodes,1:nbcnodes) = diag(H_obs_diag);
+H_obs = sparse(H_obs);
+A_forward = H_obs*A_tikh_inv;
+X0 = 1e-3*ones(size(soln_FE_n_tikh));
+Data_Bc = H_obs*soln_FE_n_tikh + noise;
+figure(10)
+plot(Data_Bc)
 options0 = optimoptions('fmincon','CheckGradients',false,'SpecifyObjectiveGradient',false,...
-    'PlotFcn','optimplotfval','MaxIterations',20000,'MaxFunctionEvaluations',1e5);
-soln_reconstructed = fmincon(@(X)costfun_FEM(X,A,Data_Bc,bndNodes,lambda),X0,[],[],[],[],[],[],[],options0);
+    'PlotFcn','optimplotfval','MaxIterations',1e7,'MaxFunctionEvaluations',1e7);
+sourceterm_reconstructed = fmincon(@(X)costfun_FEM(X,A_forward,Data_Bc,bndNodes,lambda_inv,reg_method),X0,[],[],[],[],[],[],[],options0);
 figure(651)
-Data_Estimated = A*soln_reconstructed;
-plot(Data_Estimated(bndNodes))
-
-figure(602)
-scatter(model.Geometry.Vertices(:,1),model.Geometry.Vertices(:,2))
+Soln_Inv = A_tikh_inv*sourceterm_reconstructed;
+plot(Soln_Inv(bndNodes))
 
 figure(604)
 markersize_scaleup = 100;
@@ -128,7 +142,14 @@ legend('Tikhonov regularised')
 legend show
 
 figure(1020)
-pdeplot(model,"XYData",soln_reconstructed(:,1),"FaceAlpha",0.9); hold on;
+pdeplot(model,"XYData",Soln_Inv(:,1),"FaceAlpha",0.9); hold on;
+xlabel('x')
+ylabel('y')
+legend('Inverse Solution')
+legend show
+
+figure(1021)
+pdeplot(model,"XYData",sourceterm_reconstructed(:,1),"FaceAlpha",0.9); hold on;
 xlabel('x')
 ylabel('y')
 legend('Inverse Solution')
--- costfun_FEM.m
+++ costfun_FEM.m
@@ -1,9 +1,13 @@
-function [f,g] = costfun_FEM(X,A,Data,bndNodes,lambda)
+function [f,g] = costfun_FEM(X,A,Data,bndNodes,lambda,reg_method)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-u = A*X;
-u_traced = u(bndNodes);
-f = 0.5d0*norm(u_traced-Data)^2 + 0.5d0*lambda*norm(X,2)^2; %+ (lambda/100)*norm(X,1);
+u_bc = A*X;
+
+% if reg_method == 1
+%     f = 0.5d0*norm(u_traced-Data)^2 + 0.5d0*lambda*norm(X,2)^2; %+ (lambda/100)*norm(X,1);
+% else
+    f = 0.5d0*norm(u_bc-Data)^2 + 0.5d0*lambda*norm(X,1);
+% end
 % g_tikh = (A')*(A*X-Data) + lambda*X;
 
 g= [];
