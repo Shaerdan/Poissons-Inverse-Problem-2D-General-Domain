b4da457514a2a4bcedfb201c183c423cf833133c
Shaerdan <Shaerdan@gmail.com>
2023-02-09 13:47:17 +0000

    corrected data generation and boundary tracing

--- FE_forward_solution_proto.m
+++ FE_forward_solution_proto.m
@@ -9,34 +9,18 @@ l_plot_loop = 0;
 l_grad_test = 1;
 curve_num = 1;
 n_mesh_bulk = 400;
-N_source = 10;
-% N_ob = 20;
-% N_skip = 5;
-var_noise = 0;
-% pattern = 3; % 1 for 'uniform partial', 2 for 'random', 3 for 'all'
-
-% [H] = observationOperator(n_mesh_bc,N_ob,N_skip,pattern);
+N_source = 4;
 
 for itest = 1:curve_num
     [x_curve,y_curve] = randomsmoothcurveB2(smoothsteps,rmax,rmin,n_mesh_bc,l_plot_smoothing);
     hold on;
 end
 bc_curve = [x_curve;y_curve];
-% [x_rand,y_rand] = gen_mesh_points(x_curve,y_curve,n_mesh_bulk);
 [x_source,y_source] = gen_mesh_points(x_curve,y_curve,N_source);
-
-% plot_num = 3;
-% figure(plot_num)
-% scatter(x_rand,y_rand,'.'); hold on; plot(x_curve,y_curve,'r*');
-% plot_num = plot_num +1;
 
 % generate random intensity that sums to 0;
 S_true = randn(N_source,1); S_mean = mean(S_true);
 S_true = S_true - S_mean;
-% S_true = zeros(n_mesh_bulk,1);
-% S_true(1) = 1; S_true(floor(n_mesh_bulk/2)) = -1;
-% sum(S_true)
-% Zi = [x_rand;y_rand];  z_ref = [x_curve;y_curve];
 
 
 if l_plot_conformal == 1
@@ -49,46 +33,40 @@ if l_plot_conformal == 1
     xlabel('boundary mesh indx');
     ylabel('\phi_{poly}');
     legend show
-    plot_num = plot_num + 1;
-    
+    plot_num = plot_num + 1;   
 end
-% vertices = [x_curve, y_curve; x_rand, y_rand];
-% faces = convhull(x_curve, y_curve);
-% tri = delaunayTriangulation(vertices);
+
 tri = delaunayTriangulation([x_curve, y_curve]);
-% filename1 = 'bc_geometry';
-% stlwrite(filename1, tri)% tri = [x_curve, y_curve];
+
 figure(901)
 triplot(tri);hold on;
 scatter(x_source,y_source,'r*');
-
-
+var_noise = 0;
+lambda = 0.1;
 model = createpde();
 stlwrite(tri,'tritext2D.stl','text')
 geo = importGeometry(model,"tritext2D.stl");
 
-% g = geometryFromEdges(model,@circleg);
 g = @(region,state)gfun ;
 mesh=generateMesh(model,"Hmax",.05,"GeometricOrder","linear");
-
+figure(9090)
+pdegplot(model.Geometry)
 disp(strcat('Number of Nodes = ','',num2str(length(mesh.Nodes))));
 applyBoundaryCondition(model,"neumann", ...
     "Face",1:model.Geometry.NumFaces, ...
     "g",g);
-Nf2 = 1:64;
-elemIDs = 1:60;
+bndNodes0 = boundary(mesh.Nodes(1,:)',mesh.Nodes(2,:)');
+bndNodes = unique(bndNodes0);
+
+
 figure(1010)
-scatter(mesh.Nodes(1,elemIDs)',mesh.Nodes(2,elemIDs)')
-% phi = @(xi, eta) [1-xi, 1+xi, 1-eta, 1+eta];
-% phi = @(xi, eta) [(1 - xi) * (1 - eta) / 4, (1 + xi) * (1 - eta) / 4,...
-%     (1 + xi) * (1 + eta) / 4, (1 - xi) * (1 + eta) / 4];
+scatter(mesh.Nodes(1,bndNodes)',mesh.Nodes(2,bndNodes)')
+
 phi = @(xi, eta) [(1 - xi - eta), xi, eta];
-% gaussKernel = fspecial('gaussian', [5 5], 1);
 conv_kernel = [];
-lambda = 0.1;
 tol = 1e-5;
 i_sigma = 1;
-eps = 5*1e-2;
+eps = 1*1e-1;
 f = @(location,state)fsource(location,state,S_true,x_source,y_source,N_source,eps,conv_kernel);
 specifyCoefficients(model,"m",0,...
     "d",0,...
@@ -96,27 +74,19 @@ specifyCoefficients(model,"m",0,...
     "a",0,...
     "f",f);
 FEMn = assembleFEMatrices(model,"nullspace");
+noise = sqrt(var_noise)*randn(size(bndNodes));
+
 cond(FEMn.Kc)
-%     FEMs = assembleFEMatrices(model,"stiff-spring");
-%     soln_FE_n = FEMn.B*(FEMn.Kc\FEMn.Fc) + FEMn.ud;
-%     Kinv = ridge(FEMn.Kc, lambda);
+
 tikh = FEMn.Kc'*FEMn.Kc + lambda*speye(size(FEMn.Kc));
 FcKinv = tikh\FEMn.Kc'*FEMn.Fc;
 soln_FE_n_tikh(:,1) = FEMn.B*(FcKinv) + FEMn.ud;
-%     noise = randn(size(FEMn.Fc));
-noise = 0;
-Data_Fc = FEMn.Fc + noise;
-%     soln_FE_s = FEMs.Ks\FEMs.Fs;
-results = solvepde(model,phi);
+Data_Bc = soln_FE_n_tikh(bndNodes,1) + noise;
+figure(10)
+plot(Data_Bc)
 
-% extract boundary solution:
-phi_forward_FE_raw(:,1) = results.NodalSolution(Nf2);
-phi_forward_FE_tikh(:,1) = soln_FE_n_tikh(Nf2,1);
-
-soln_FE_raw(:,1) = results.NodalSolution;
 if l_plot_loop == 1
     figure(plot_num)
-    pdeplot(model,"XYData",results.NodalSolution)
     legend('unregularised')
     legend show
     plot_num = plot_num + 1;
@@ -135,42 +105,17 @@ end
 A = FEMn.Kc;
 % A_Tikh = FEMn.Kc'*FEMn.Kc + lambda*speye(size(FEMn.Kc));
 % Data_Fc_Tikh = FEMn.Kc'*Data_Fc;
-X0 = rand(size(Data_Fc));
-figure(650)
-lambda = 0.1;
-plot(Data_Fc)
+X0 = rand(size(soln_FE_n_tikh));
+
 options0 = optimoptions('fmincon','CheckGradients',false,'SpecifyObjectiveGradient',false,...
     'PlotFcn','optimplotfval','MaxIterations',20000,'MaxFunctionEvaluations',1e5);
-soln_reconstructed = fmincon(@(X)costfun_FEM(X,A,Data_Fc,lambda),X0,[],[],[],[],[],[],[],options0);
+soln_reconstructed = fmincon(@(X)costfun_FEM(X,A,Data_Bc,bndNodes,lambda),X0,[],[],[],[],[],[],[],options0);
 figure(651)
-plot(A*soln_reconstructed)
-% soln_reconstructed_Tikh = fmincon(@(X)costfun_FEM(X,A_Tikh,Data_Fc_Tikh),X0,[],[],[],[],[],[],[],options0);
-% figure(1021)
-% pdeplot(model,"XYData",soln_reconstructed_Tikh(:,1),"FaceAlpha",0.9); hold on;
-
-figure(600)
-plot(phi_forward_FE_raw(:,1),'DisplayName',strcat('Unregularised, eps =',eps)); hold on;
-
-xlabel('boundary nodes')
-ylabel('\phi(\partial \Omega)')
-legend show
-
-
-figure(601)
-plot(phi_forward_FE_tikh(:,1),'DisplayName',strcat('Tikhonov regularised, eps =',eps)); hold on;
-xlabel('boundary nodes')
-ylabel('\phi(\partial \Omega)')
-legend show
+Data_Estimated = A*soln_reconstructed;
+plot(Data_Estimated(bndNodes))
 
 figure(602)
 scatter(model.Geometry.Vertices(:,1),model.Geometry.Vertices(:,2))
-
-figure(603)
-pdeplot(model,"XYData",soln_FE_raw(:,1))
-xlabel('x')
-ylabel('y')
-legend('unregularised')
-legend show
 
 figure(604)
 markersize_scaleup = 100;
--- costfun_FEM.m
+++ costfun_FEM.m
@@ -1,7 +1,11 @@
-function [f,g] = costfun_FEM(X,A,Data_Fc,lambda)
+function [f,g] = costfun_FEM(X,A,Data,bndNodes,lambda)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-f = 0.5d0*norm(A*X-Data_Fc)^2 + 0.5d0*lambda*norm(X,2)^2 %+ (lambda/100)*norm(X,1);
-g = [];
+u = A*X;
+u_traced = u(bndNodes);
+f = 0.5d0*norm(u_traced-Data)^2 + 0.5d0*lambda*norm(X,2)^2; %+ (lambda/100)*norm(X,1);
+% g_tikh = (A')*(A*X-Data) + lambda*X;
+
+g= [];
 end
 
