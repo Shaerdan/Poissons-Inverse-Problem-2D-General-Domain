3341c4993453a557f6acc064c61bfcfec47096ee
Shaerdan <Shaerdan@gmail.com>
2023-02-14 01:13:50 +0000

    TV regularisation added

--- costfun_FEM.m
+++ costfun_FEM.m
@@ -1,13 +1,17 @@
-function [f,g] = costfun_FEM(X,A,Data,bndNodes,lambda,reg_method)
+function [f,g] = costfun_FEM(X,A,Data,bndNodes,lambda,reg_method,model)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 u_bc = A*X;
-
+X_field =createPDEResults(model,X);
 % if reg_method == 1
 %     f = 0.5d0*norm(u_traced-Data)^2 + 0.5d0*lambda*norm(X,2)^2; %+ (lambda/100)*norm(X,1);
-% else
-    f = 0.5d0*norm(u_bc-Data)^2 + 0.5d0*lambda*norm(X,1);
-% end
+% elseif reg_method == 2
+%     f = 0.5d0*norm(u_bc-Data)^2 + 0.5d0*lambda*norm(X,1);
+% elseif reg_method == 3
+X_tv = sqrt(X_field.XGradients.^2 + X_field.YGradients.^2);
+f = 0.5d0*norm(u_bc-Data)^2 + 0.5d0*lambda*norm(X_tv,2);
+%end
+
 % g_tikh = (A')*(A*X-Data) + lambda*X;
 
 g= [];
